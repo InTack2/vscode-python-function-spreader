31f468125bf5839b1f9974ca8088dc1d12a701f8
Tack2 <takumi236@gmail.com>
2021-07-09 23:16:59 +0900

    refactor main logic

--- extension.js
+++ extension.js
@@ -6,8 +6,6 @@ const vscode = require('vscode');
 function activate(context) {
     console.log('Congratulations, your extension "python-print-spreader" is now active!');
 
-    // let python_print_template = 'print("${word}", ${word})'
-
     let disposable = vscode.commands.registerCommand('python-print-spreader.helloWorld', function () {
         const config = vscode.workspace.getConfiguration("python-print-spreader");
 
@@ -28,38 +26,35 @@ function deactivate() { }
 async function selection_word_choice_function_spreader(python_template) {
     const editor = vscode.window.activeTextEditor;
 
-    if (editor) {
-        const document = editor.document;
+    if (!editor) { return; }
+    const document = editor.document;
 
-        if (!editor.selection.isEmpty) {
-            const isSingle = editor.selection.isSingleLine;
-            console.log("isSingle", isSingle);
+    if (editor.selection.isEmpty) { return; }
 
-            // 複数行をまとめて選択してないか？
-            if (editor.selection.isSingleLine) {
 
-                let sorted_selection_list = editor.selections.sort();
-                // 選択範囲
-                for (let selection_index in sorted_selection_list) {
+    if (editor.selection.isSingleLine) {
 
-                    let selection_position = sorted_selection_list[selection_index];
+        let sorted_selection_list = editor.selections.sort(function (a, b) {
+            if (a.start.line < b.start.line) return 1;
+            if (a.start.line > b.start.line) return -1;
+            return 0;
+        });
 
-                    const selection_range = new vscode.Range(selection_position.start, selection_position.end);
-                    console.log("start", selection_position.start);
-                    console.log("end", selection_position.end);
+        for (let selection_index in sorted_selection_list) {
+            let selection_position = sorted_selection_list[selection_index];
 
-                    let word = document.getText(selection_range);
+            if (selection_position.start.character == selection_position.end.character) { continue; }
 
-                    let python_command = "\n" + eval("`" + python_template + "`");
+            const selection_range = new vscode.Range(selection_position.start, selection_position.end);
 
-                    let python_snippet = new vscode.SnippetString(python_command);
+            let word = document.getText(selection_range);
 
-                    let insert_position = new vscode.Position(selection_position.start.line, selection_position.end.character);
+            let python_command = "\n" + eval("`" + python_template + "`");
 
-                    editor.insertSnippet(python_snippet, insert_position, { "undoStopAfter": false, "undoStopBefore": false });
-                    // 前か後かで選べても便利そう。
-                }
-            }
+            let python_snippet = new vscode.SnippetString(python_command);
+            let insert_position = new vscode.Position(selection_position.start.line, selection_position.end.character);
+
+            editor.insertSnippet(python_snippet, insert_position, { "undoStopAfter": false, "undoStopBefore": false });
         }
     }
 }
