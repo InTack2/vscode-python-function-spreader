53ddfe08577494b14059bba5b6bdfe2563972a24
Tack2 <takumi236@gmail.com>
2021-07-09 22:29:53 +0900

    add base extension

--- extension.js
+++ extension.js
@@ -4,43 +4,67 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	console.log('Congratulations, your extension "python-print-spreader" is now active!');
+    console.log('Congratulations, your extension "python-print-spreader" is now active!');
 
-	let disposable = vscode.commands.registerCommand('python-print-spreader.helloWorld', function () {
-		const editor = vscode.window.activeTextEditor;
+    // let python_print_template = 'print("${word}", ${word})'
 
-		if (editor) {
-			const document = editor.document;
+    let disposable = vscode.commands.registerCommand('python-print-spreader.helloWorld', function () {
+        const config = vscode.workspace.getConfiguration("python-print-spreader");
 
-			if (!editor.selection.isEmpty) {
-				const isSingle = editor.selection.isSingleLine;
-				console.log("isSingle", isSingle);
+        const python_template_list = config.get("target-function");
 
-				// 複数行をまとめて選択してないか？
-				if (editor.selection.isSingleLine) {
-					// 選択範囲
-					for (let selection_index in editor.selections) {
-						console.log("selection_index", selection_index);
+        vscode.window.showQuickPick(python_template_list)
+            .then(result => { vscode.window.showInformationMessage(`Call  ${result}`); return result; })
+            .then(result => { selection_word_choice_function_spreader(result) });
 
-						const selection_range = new vscode.Range(editor.selections[selection_index].start, editor.selections[selection_index].end);
-						let word = document.getText(selection_range);
-						console.log("word", word);
+        vscode.window.showInformationMessage('Hello World from PythonPrintSpreader!');
+    });
 
-						// ワードは取れてるので後は改行して選択対象を含んだprint文を次の行に挿入する。
-						// 前か後かで選べても便利そう。
-					}
-				}
-			}
-		}
-		vscode.window.showInformationMessage('Hello World from PythonPrintSpreader!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
 function deactivate() { }
 
+async function selection_word_choice_function_spreader(python_template) {
+    const editor = vscode.window.activeTextEditor;
+
+    if (editor) {
+        const document = editor.document;
+
+        if (!editor.selection.isEmpty) {
+            const isSingle = editor.selection.isSingleLine;
+            console.log("isSingle", isSingle);
+
+            // 複数行をまとめて選択してないか？
+            if (editor.selection.isSingleLine) {
+
+                let sorted_selection_list = editor.selections.sort();
+                // 選択範囲
+                for (let selection_index in sorted_selection_list) {
+
+                    let selection_position = sorted_selection_list[selection_index];
+
+                    const selection_range = new vscode.Range(selection_position.start, selection_position.end);
+                    console.log("start", selection_position.start);
+                    console.log("end", selection_position.end);
+
+                    let word = document.getText(selection_range);
+
+                    let python_command = "\n" + eval("`" + python_template + "`");
+
+                    let python_snippet = new vscode.SnippetString(python_command);
+
+                    let insert_position = new vscode.Position(selection_position.start.line, selection_position.end.character);
+
+                    editor.insertSnippet(python_snippet, insert_position, { "undoStopAfter": false, "undoStopBefore": false });
+                    // 前か後かで選べても便利そう。
+                }
+            }
+        }
+    }
+}
+
 module.exports = {
-	activate,
-	deactivate
+    activate,
+    deactivate
 }
